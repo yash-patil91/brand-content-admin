4e1b4657527850bdd54f9944d4c652d042fd10c3
yash patil <patilyash445@gmail.com>
2024-07-09 12:49:02 +0530

    added logo

--- src/components/CampaignManagement .js
+++ src/components/CampaignManagement .js
@@ -32,30 +32,30 @@ const CampaignManagement = () => {
     };
 
     return (
-        <><div>
-            <div className="bg-white shadow-sm w-full px-3 sm:px-4 md:px-8 lg:px-8 py-3 flex justify-between">
-                <div>
-                    <img
-                        className="h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                    />
+        <>
+         <div className="bg-white shadow-sm w-full px-3 sm:px-4 md:px-8 lg:px-8 flex justify-between items-center">
+            <div>
+                <img
+                    className="h-24 cursor-pointer"
+                    src={require("../assets/images/Logo.png")}
+                    alt="Your Company"
+                    // onClick={() => navigate('/')}
+                />
+            </div>
+            <div className="relative">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    required
+                    className="block w-full sm:w-96 md:w-96 lg:w-96 rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none"
+                />
+                <div className="absolute top-1 right-2">
+                    <SearchOutlinedIcon style={{ fontSize: "18px", color: "slategray" }} />
                 </div>
-                <div className="relative">
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        required
-                        className="block xs:w-full sm:w-96 md:w-96 lg:w-96 rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none"
-                    />
-                    <div className="absolute top-1 right-2">
-                        <SearchOutlinedIcon style={{ fontSize: "18px", color: "slategray" }} />
-                    </div>
-                </div>
-                <div>
-                    <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full border border-slate-300 cursor-pointer">
-                        <span className="font-medium text-slate-500 ">JL</span>
-                    </div>
+            </div>
+            <div>
+                <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full border border-slate-300 cursor-pointer">
+                    <span className="font-medium text-slate-500">JL</span>
                 </div>
             </div>
         </div>
